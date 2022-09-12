453e8bdc503b2d424ccb854c396bfad379261afc
José Vinicius Siqueira da Silva <jose.vinicius@hexait.com.br>
2022-09-12 20:26:21 -0300

    manipulado múltiplas requisições em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map(
+    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
+  );
+}
+
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join("")}
             </ol>
 
             <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                src="${pokemon.sprites.other.dream_world.front_default}"
                 alt="${pokemon.name}"
             />
         </div>
@@ -21,5 +26,6 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = newHtml;
 });
