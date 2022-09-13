e3e1db3b5de0a3816a13760fa3cd763d2b659f7e
José Vinicius Siqueira da Silva <jose.vinicius@hexait.com.br>
2022-09-12 21:00:51 -0300

    Convertendo o model do PokeApi para o meu modelo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,18 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-  return pokemonTypes.map(
-    (typeSlot) => `<li class="type">${typeSlot.type.name}</li>`
-  );
-}
-
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon">
-        <span class="number">#${pokemon.order}</span>
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                ${convertPokemonTypesToLi(pokemon.types).join("")}
+                ${pokemon.types
+                  .map((type) => `<li class="type">${type}</li>`)
+                  .join("")}
             </ol>
 
             <img
-                src="${pokemon.sprites.other.dream_world.front_default}"
+                src="${pokemon.photo}"
                 alt="${pokemon.name}"
             />
         </div>
