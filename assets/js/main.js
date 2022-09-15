c3fceb39d75d8efcbf7a914b46e96895cdb62069
Vinicius Siqueira <viniciussiqueira.dev@gmail.com>
2022-09-14 21:28:57 -0300

    criando mecanismo para limitar na primeira geracao de pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
-const limit = 5;
+
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
@@ -32,5 +34,14 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener("click", () => {
   offset += limit;
-  loadPokemonItens(offset, limit);
+  const qtdRecordNextPage = offset + limit;
+
+  if (qtdRecordNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
 });
